f346137d4d038ee87a4950b936cc3a69e2c21703
Карим Искаков <karfly@MacBook-Air-Karim.local>
2017-02-28 14:47:35 +0300

    Added gold_speed features

--- dota_ml_utils.py
+++ dota_ml_utils.py
@@ -1,17 +1,20 @@
 import os
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 
 def transform_data(data_dir_path,
                    last_gold_by_player=True,
-                   last_gold_by_team=True):
+                   last_gold_by_team=True,
+                   gold_speed_by_player=True,
+                   gold_speed_by_team=True):
     train_df = pd.read_csv(os.path.join(data_dir_path, 'train.csv'), index_col='mid')
     test_df = pd.read_csv(os.path.join(data_dir_path, 'test.csv'), index_col='mid')
 
     player_columns = ['player_{}'.format(i) for i in range(10)]
-    radiat_player_columns = player_columns[:5]
+    radiant_player_columns = player_columns[:5]
     dire_player_columns = player_columns[5:]
 
     if last_gold_by_player:
@@ -30,7 +33,7 @@ def transform_data(data_dir_path,
                 player_columns].add_prefix('last_gold_')
 
         last_gold_radiant_df = last_gold_by_player_df[
-            ['last_gold_{}'.format(player) for player in radiat_player_columns]].sum(axis=1).to_frame().rename(
+            ['last_gold_{}'.format(player) for player in radiant_player_columns]].sum(axis=1).to_frame().rename(
             columns={0: 'last_gold_radiant'})
         last_gold_dire_df = last_gold_by_player_df[
             ['last_gold_{}'.format(player) for player in dire_player_columns]].sum(axis=1).to_frame().rename(
@@ -39,6 +42,34 @@ def transform_data(data_dir_path,
         last_gold_by_team_df = pd.merge(last_gold_radiant_df, last_gold_dire_df, left_index=True, right_index=True)
         train_df = pd.merge(train_df, last_gold_by_team_df, left_index=True, right_index=True)
         test_df = pd.merge(test_df, last_gold_by_team_df, left_index=True, right_index=True)
+
+    if gold_speed_by_player:
+        print('Adding \'gold_speed_by_player\'...')
+        gold_df = pd.read_csv(os.path.join(data_dir_path, 'gold.csv'), index_col='mid')
+        gold_speed_by_player_df = gold_df.groupby(gold_df.index).agg(lambda x: np.diff(x.values).mean())[
+            player_columns].add_prefix('gold_speed_')
+        train_df = pd.merge(train_df, gold_speed_by_player_df, left_index=True, right_index=True)
+        test_df = pd.merge(test_df, gold_speed_by_player_df, left_index=True, right_index=True)
+
+    if gold_speed_by_team:
+        print('Adding \'gold_speed_by_team\'...')
+        if not gold_speed_by_player:
+            gold_df = pd.read_csv(os.path.join(data_dir_path, 'gold.csv'), index_col='mid')
+            gold_speed_by_player_df = gold_df.groupby(gold_df.index).agg(lambda x: np.diff(x.values).mean())[
+                player_columns].add_prefix('gold_speed_')
+
+        gold_speed_radiant_df = gold_speed_by_player_df[
+            ['gold_speed_{}'.format(player) for player in radiant_player_columns]].mean(axis=1).to_frame().rename(
+            columns={0: 'gold_speed_radiant'})
+
+        gold_speed_dire_df = gold_speed_by_player_df[
+            ['gold_speed_{}'.format(player) for player in dire_player_columns]].mean(axis=1).to_frame().rename(
+            columns={0: 'gold_speed_dire'})
+
+        gold_speed_by_team_df = pd.merge(gold_speed_radiant_df, gold_speed_dire_df, left_index=True, right_index=True)
+        train_df = pd.merge(train_df, gold_speed_by_team_df, left_index=True, right_index=True)
+        test_df = pd.merge(test_df, gold_speed_by_team_df, left_index=True, right_index=True)
+
 
     return train_df, test_df
 
